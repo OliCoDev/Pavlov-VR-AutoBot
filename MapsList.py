4302dcd93e49d9c4ef2d4607f7e2013dbcf9a84f
OliCoDev <110675229+OliCoDev@users.noreply.github.com>
2022-08-08 15:42:46 +0200

    Added Map, MapsList and Converter classes

---
+++ MapsList.py
@@ -0,0 +1,22 @@
+import Map
+import random
+
+maps = []
+
+def getMap(self):
+    global maps
+    if len(maps) == 0:
+        return None
+    return maps.pop(0)
+
+def addMap(newMap):
+    global maps
+    maps.append(newMap)
+
+def randomiseOrder():
+    global maps
+    for i in range(len(maps)):
+        randomPos = random.randint(0, len(maps)-1)
+        tempMap = maps[randomPos]
+        maps[randomPos] = maps[i]
+        maps[i] = tempMap
